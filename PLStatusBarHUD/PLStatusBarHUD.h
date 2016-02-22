1ee27ff9f23b64c0f877de67af6c8477913536a0
pzl <pangzhanli2009@163.com>
2016-02-22 16:16:44 +0800

    给各个方法添加注释
    给各个方法添加注释

--- PLStatusBarHUD/PLStatusBarHUD.h
+++ PLStatusBarHUD/PLStatusBarHUD.h
@@ -10,12 +10,48 @@
 
 @interface PLStatusBarHUD : NSObject
 
+/**
+ *  显示带有文字和图片的消息
+ *
+ *  @param msg      文字
+ *  @param image    图片
+ *
+ */
 +(void)showMessage:(NSString *)msg image:(UIImage *)image;
+
+/**
+ *  显示成功消息
+ *
+ *  @param msg      成功消息
+ */
 +(void)showSuccess:(NSString *)msg;
+
+/**
+ *  显示错误消息
+ *
+ *  @param msg      错误消息
+ */
 +(void)showError:(NSString *)msg;
+
+/**
+ *  显示正在加载信息
+ *
+ *  @param msg      文字
+ */
 +(void)showLoading:(NSString *)msg;
+
+/**
+ *  显示普通消息
+ *
+ *  @param msg      文字
+ */
 +(void)showMessage:(NSString *)msg;
 
+
+/**
+ *  隐藏
+ *
+ */
 +(void)hide;
 
 @end
